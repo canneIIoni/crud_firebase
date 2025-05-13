fa77a741f0f8762641d5ae5ece2b93653f55d0e8
Luca <paesberaldiluca@gmail.com>
2025-05-13 12:47:49 -0300

    setup the firebase functions on the terminal, created firestore.dart with its basic functions, made a simple home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/services/firestore.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+
+  // get collection of notes
+  final CollectionReference notes = FirebaseFirestore.instance.collection('notes');
+
+  // CREATE: adding a new note
+  Future<void> addNote(String note) {
+    return notes.add({
+      'note': note,
+      'time': Timestamp.now()
+    });
+  }
+
+  // READ: get notes from database
+
+  // UPDATE: update notes given a doc id
+
+  // DELETE: delete notes given a doc id
+
+}
